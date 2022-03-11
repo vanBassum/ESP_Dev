04938c27d4c2907b0f113fd9ed5311907d4452ab
Bas Visscher <bas.visscher24@gmail.com>
2022-03-11 21:44:31 +0100

    Implemented cobs decoding, not tested yet!!!

--- main/main.cpp
+++ main/main.cpp
@@ -8,28 +8,13 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 #include "ESP_log.h"
-#include "hx711.h"
-#include <list>
 #include <string.h>
-#include "lib/tft/tft.h"
-#include "filter.h"
-#include "settings.h"
-#include "lib/tcpip/tcpsocket.h"
-#include "lib/misc/datetime.h"
-#include "lib/rtos/queue.h"
-#include "esp_timer.h"
-#include "sample.h"
-#include "sendsamples.h"
+
+#include "lib/rtos/stream.h"
+
 
 #define SSID "vanBassum"
 #define PSWD "pedaalemmerzak"
-
-HX711 hx711(GPIO_NUM_21, GPIO_NUM_19);
-Filter<uint32_t> scaleFilter(64);
-TFT tft = TFT::Get_ILI9341();
-Font* font = 0;
-
-
 
 extern "C" {
 	void app_main();
@@ -65,102 +50,90 @@ void StartWIFI()
 }
 
 
-void TakeSample(Sample* sample, int samples)
-{
-	uint64_t bigMeas = 0;
-	for (int i = 0; i < samples; i++)
-		bigMeas += hx711.Read();
-	sample->Set(bigMeas / samples);
-	sample->Samples = samples;
-}
-
-void DisplaySample(Sample s, Color c)
-{
-	char buf[32];
-	snprintf(buf, 32, "%d    ", s.Raw);
-	tft.DrawString(font, 0, 64, buf, c);
-	snprintf(buf, 32, "%f    ", s.Weight);
-	tft.DrawString(font, 0, 64 + 32, buf, c);
-}
-
-enum class State : uint8_t
-{
-	Standby,
-	WaitForstable,
-	CollectSample,
-	Cooldown,
-};
-
 void app_main(void)
 {
 	nvs_flash_init();
 	StartWIFI();
-	tft.Init();
-	font = new Font(ILGH32XB);
-	tft.FillScreen(Color(0, 0, 0));
-	tft.SetFontFill(Color(0, 0, 0));
-	hx711.SetGain(2);
-	InitSendSamples();
-
-	Sample sample;
-	State prvState = State::Standby;
-	State actState = State::Standby;
-	State nxtState = State::Standby;
-	bool onEntry = true;
-	while (true)
-	{
-
-		switch (actState)
-		{
-		case State::Standby:
-			if (onEntry)
-				tft.BacklightOff();
-			tft.DrawString(font, 0, 128, "Standby        ", Color(255, 0, 0));
-			DisplaySample(sample, Color(255, 0, 0));
-			TakeSample(&sample, 5);
-			if (sample.Weight > 2)
-				nxtState = State::WaitForstable;
-			else
-				vTaskDelay(100/portTICK_PERIOD_MS);
-			break;
-
-		case State::WaitForstable:
-			if (onEntry)
-				tft.BacklightOn();
-			tft.DrawString(font, 0, 128, "WaitForstable  ", Color(255, 0, 0));
-			TakeSample(&sample, 5);
-			DisplaySample(sample, Color(255, 0, 0));
-			if (sample.Weight > 70)
-				nxtState = State::CollectSample;
-			else if (sample.Weight < 2)
-				nxtState = State::Standby;
-			break;
-
-		case State::CollectSample:
-			tft.DrawString(font, 0, 128, "CollectSample  ", Color(255, 0, 0));
-			TakeSample(&sample, 20);
-			DisplaySample(sample, Color(0, 255, 0));
-			EnqueueSample(sample);
-			nxtState = State::Cooldown;
-			break;
-
-		case State::Cooldown:
-			tft.DrawString(font, 0, 128, "Cooldown       ", Color(255, 0, 0));
-			TakeSample(&sample, 5);
-			if (sample.Weight < 2)
-				nxtState = State::Standby;
-			else
-				vTaskDelay(100/portTICK_PERIOD_MS);
-			break;
-		}
-
-		onEntry = false;
-		if (actState != nxtState)
-		{
-			prvState = actState;
-			actState = nxtState;
-			onEntry = true;
-		}
-	}
+	
 }
+
+
+namespace Framing
+{
+	class COBS
+	{
+		class Decoder
+		{
+			FreeRTOS::MessageBuffer& OutputStream;
+			FreeRTOS::StreamBuffer& InputBuffer; 
+			bool memOverflow = false;
+			uint8_t nextZero = 0;
+			uint8_t* buffer = NULL;
+			size_t bufferPtr = 0;
+			size_t bufferSize = 0;
+			
+		public:
+			void Init(FreeRTOS::MessageBuffer& outputStream, FreeRTOS::StreamBuffer& inputBuffer, size_t inputBufferSize)
+			{
+				if (inputBufferSize < 255)
+					bufferSize = 255;
+				else
+					bufferSize = inputBufferSize;
+				
+				OutputStream = outputStream;
+				InputBuffer = inputBuffer;
+				buffer = (uint8_t *)malloc(bufferSize);
+			}
+			
+			void Work(TickType_t wait)
+			{
+				//Sanity check!
+				if (bufferSize >= 255 && buffer != NULL)
+				{
+					if (nextZero == 0) //Start of frame, First byte shows where the next zero is.
+					{
+						memOverflow = false;
+						bufferPtr = 0;
+						nextZero = 1;
+					}
+					if (bufferPtr + nextZero > bufferSize)	
+					{
+						ESP_LOGE("COBS_Decode", "Message larger than buffer");
+						memOverflow = true;
+						bufferPtr = 0;				//We still need to read the rest of the data, untill a 0 has been discovered. Since the buffer is at least 255, this will ensure enough bytes can be read.
+					}
+					InputBuffer.SetTriggerLevel(nextZero);
+					size_t len = InputBuffer.Receive(&buffer[bufferPtr], nextZero, wait);
+					bufferPtr += len;
+					nextZero -= len;
+					if (nextZero == 0)
+					{
+						nextZero = buffer[bufferPtr - 1];
+						if (nextZero == 0)
+						{
+							//Full messge received
+							if (!memOverflow)
+							{
+								if (OutputStream.Send(buffer, bufferPtr + 1, wait) != bufferPtr + 1)
+									ESP_LOGE("COBS_Decode", "Not enough memory in output stream");
+							}
+							else
+								ESP_LOGE("COBS_Decode", "End of message found, but buffer overflowed. Message discarded");
+						}
+						else
+							bufferPtr--; //Overwrite the nextZeroPointer with more data.
+					}
+				}
+				else
+					ESP_LOGE("COBS_Decode", "Not initialized");
+			}
+		};
+	};
+}
+
+
+
+
+
+
 
